7e2b281efd714aca878d04ae08b351e4aeced34e
Lucas Takanori <lucas.takanori.sanchez@estudiantat.upc.edu>
2023-04-19 06:22:01 +0200

    main

---
+++ TFG/main.py
@@ -0,0 +1,12 @@
+#input csv amb path, text --> N arxius a obrir
+#Demanar P metodes a fer servir
+#Demanar M cops a agumentar (aplicar Mx(N-1) cops)
+#importar P metodes
+#Llista N arxius-> asignar aleatoriament 1 metode 
+#amb parametres aleatoris a 1 arxiu repetir fins arribar a M x(N-1) -> 
+#->llista Mx(N-1) operacions
+#ordenar llista per nom
+#aplicar arxiu per arxiu K augmentacions (guardar nom arxiu nou, tipus augmentació, 
+# label dificultat, parametres utilitzats)
+#repetir per N arxius 
+#guardar en csv NXM --path --text --score --tipus --parametres 
