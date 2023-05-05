ffada7ed9fbf35983d625ee387b2263ed5b4d35a
LucasTakanori <79314835+LucasTakanori@users.noreply.github.com>
2023-05-05 05:24:00 +0200

    Revert "PESQ"
    This reverts commit b2c4339f05a50d8cdf81d4d2966934ae1158dd15.

--- TFG/PESQ.py
+++ TFG/PESQ.py
@@ -2,8 +2,6 @@ import torch
 from torchmetrics.functional.audio.pesq import perceptual_evaluation_speech_quality
 from scipy.io import wavfile
 import warnings
-from pesq import cypesq
-
 # Suppress the specific warning messages
 warnings.filterwarnings("ignore", message="To copy construct from a tensor, it is recommended to use sourceTensor.clone().detach() or sourceTensor.clone().detach().requires_grad_(True), rather than torch.tensor(sourceTensor).")
 
@@ -21,13 +19,9 @@ def calculate_pesq(fs, ref_audio, deg_audio, mode):
     ref_audio_detached = ref_audio.detach().cpu()
     deg_audio_detached = deg_audio.detach().cpu()
     
-    try:
-        # Call the perceptual_evaluation_speech_quality function with PyTorch tensors
-        return perceptual_evaluation_speech_quality(ref_audio_detached, deg_audio_detached, fs, mode)
-    except cypesq.NoUtterancesError:
-        print("No utterances detected in the input audio files.")
-        return -1
-    
+    # Call the perceptual_evaluation_speech_quality function with PyTorch tensors
+    return perceptual_evaluation_speech_quality(ref_audio_detached, deg_audio_detached, fs, mode)
+
 def pesq_from_paths(ref_file_path, deg_file_path):
     fs_ref, ref_audio = read_audio_file(ref_file_path)
     fs_deg, deg_audio = read_audio_file(deg_file_path)
@@ -37,15 +31,16 @@ def pesq_from_paths(ref_file_path, deg_file_path):
     # Choose the mode based on the sampling frequency
     mode = 'wb' if fs_ref == 16000 else 'nb'
 
+    import numpy as np
+
     if len(ref_audio) > len(deg_audio):
-        pad_length = len(ref_audio) - len(deg_audio)
-        deg_audio = torch.cat([deg_audio, torch.zeros(pad_length, dtype=torch.float32)])
+        deg_audio = np.pad(deg_audio, (0, len(ref_audio) - len(deg_audio)))
     elif len(ref_audio) < len(deg_audio):
         deg_audio = deg_audio[:len(ref_audio)]
 
     # Calculate PESQ score
     pesq_score = calculate_pesq(fs_ref, ref_audio, deg_audio, mode)
-    return pesq_score
+    return pesq_score.item()
 
 #ref_file_path = '/home/lucastakanori/DA_programV2/testfiles/tt/common_voice_ca_17368883.wav'
 #deg_file_path = '/home/lucastakanori/DA_programV2/testfiles/tt/common_voice_ca_17368883_clipping_2.wav'
